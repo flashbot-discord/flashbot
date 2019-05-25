ac885219e35caab7ccee8b114c5b01ab7643d63f
comjun04 <30339539+comjun04@users.noreply.github.com>
2019-05-25 16:05:27 +0900

    here we go

--- bot.js
+++ bot.js
@@ -41,5 +41,7 @@ client.on('message', msg => {
 // web server
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.use(express.static('public'));
-app.listen(5000, () => console.log('Web server on port 5000'));
+app.listen(PORT, () => console.log('Web server on port ' + PORT));
