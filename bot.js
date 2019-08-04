2ac78124bd04eb00e90a2120f2963cbd1b23dbd6
comjun04 <30339539+comjun04@users.noreply.github.com>
2019-08-04 19:42:54 +0900

    lots changed

--- bot.js
+++ bot.js
@@ -3,47 +3,94 @@
  * @description Main Bot Script
  */
 
-const config = require('./config.json');
+/**
+ * config file
+ */
+let configFile;
 
+// import needed modules
 const fs = require('fs');
 const path = require('path');
-const Discord = require('discord.js');
 const Commando = require('discord.js-commando');
+
+if (fs.existsSync('./config.json')) {
+    /**
+     * config file
+     */
+    configFile = require('./config.json');
+}
+
+const config = {
+    owner: configFile.owner || process.env.owner,
+    commandPrefix: configFile.prefix || process.env.prefix,
+    db: configFile.db || process.env.db
+}
+
+/**
+ * Main Client
+ * @type {CommandoClient}
+ */
 const client = new Commando.Client({
     owner: config.owner,
-    commandPrefix: config.prefix,
+    commandPrefix: config.commandPrefix,
     unknownCommandResponse: false
 });
 
-const db = require('./firebase');
+/**
+ * Database
+ */
+switch (config.db) {
+    case 'json': {
+        let req = require('./db/json');
+        var db = new req('./db/db.json');
+        client.setProvider(db);
+    }
+}
 
+/**
+ * i18n
+ */
 const i18n = require('i18n');
 i18n.configure({
     directory: './lang',
     objectNotation: true
 });
 
+/**
+ * Bot token
+ * @type {string}
+ * @private
+ */
 const token = process.env.token || require('./token.json').token;
 
+// Ready event
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 
-    client.user.setPresence({ status: 'online', game: { name: `${config.prefix}help` } });
+    client.user.setPresence({ status: 'online', game: { name: `${config.commandPrefix}help` } });
 });
 
+// Login to Discord
 client.login(token);
 
+// Commando: Register Defaults
 client.registry
     .registerDefaults()
     .registerGroups([
         ['dev', 'Commands for developing'],
-        ['misc', 'Misc']
+        ['misc', 'Misc'],
+        ['info', 'Provides several informations'],
+        ['activation', 'activating/deactivating the bot on the server']
     ])
     .registerCommandsIn(path.join(__dirname, 'commands'));
 
-// web server
+// web server (Heroku web dyno placeholder)
 const express = require('express');
 const app = express();
+/**
+ * web server port
+ * @type {number}
+ */
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static('public'));
