4fc23c4bef14fc64b29d527de10cc8c13ca9e481
comjun04 <30339539+comjun04@users.noreply.github.com>
2019-08-31 23:04:47 +0900

    activate command

--- commands/activation/activate.js
+++ commands/activation/activate.js
@@ -11,14 +11,76 @@ module.exports = class ActivateCommand extends Command {
             description: 'Activate this bot on the server',
             userPermissions: ['ADMINISTRATOR'],
             guildOnly: true
-        })
+        });
+
+        this.result = false;
+        this.author = '';
     }
 
-    run(msg) {
+    async run(msg) {
+
+        const mcFilter = (msg) => {
+            if(this.author === msg.author.id) {
+                if(msg.content.toLowerCase() !== 'yes' && msg.content.toLowerCase() !== 'no') return false;
+                else if(msg.content.toLowerCase() === 'yes') this.result = true;
+                else if(msg.content.toLowerCase() === 'no') this.result = false;
+                return true;
+            } else return false;
+        };
+
+        const rcFilter = (reaction, user) => {
+            if (user.id === msg.author.id) {
+                if (reaction.emoji.name !== '✅' && reaction.emoji.name !== '❌') return false;
+                else if (reaction.emoji.name === '✅') this.result = true;
+                else if (reaction.emoji.name === '❌') this.result = false;
+                return true;
+            } else return false;
+        };
+
+        // testing
         msg.client.provider.set(msg.guild, 'activate', true);
 
-        msg.channel.send(i18n.__('commands.activate.execute.title') + '\n\n'
-            + i18n.__('commands.activate.execute.content.1') + '\n' + i18n.__('commands.activate.execute.content.2') + '\n' + i18n.__('commands.activate.execute.content.3') + '\n\n' 
-            + i18n.__('commands.activate.execute.content.4'));
+        const botMsg = await msg.channel.send(i18n.__('commands.activate.execute.title') + '\n\n'
+            + i18n.__('commands.activate.execute.content') + '\n\n' + i18n.__('commands.activate.execute.confirm'));
+
+        await botMsg.react('✅');
+        await botMsg.react('❌');
+
+        // Message Collector
+        this.author = msg.author.id;
+        const mc = msg.channel.createMessageCollector(mcFilter);
+        mc.on('collect', () => {
+            if (this.result) {
+                this.agree(msg, mc);
+            } else {
+                this.deny(msg, mc);
+            }
+        });
+
+        // Reaction Collector
+        const rc = botMsg.createReactionCollector(rcFilter);
+        rc.on('collect', () => {
+            if (this.result) {
+                this.agree(msg, rc);
+            } else {
+                this.deny(msg, rc);
+            }
+        });
+    }
+
+    async agree(msg, collector) {
+        // Activation
+        await console.log(`[Bot Activation] ${msg.author.tag} (${msg.member.nickname}) activated the bot in ${msg.guild.name}`);
+
+        await msg.client.provider.set(msg.guild, 'activate', true);
+
+        // Done!
+        await msg.channel.send(i18n.__('commands.activate.execute.agree'));
+        await collector.stop();
+    }
+
+    async deny(msg, collector) {
+        await msg.channel.send(i18n.__('commands.activate.execute.deny'));
+        await collector.stop();
     }
 }
