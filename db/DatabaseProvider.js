0f2bc9376c4c9056684d74fd31cd66a049964b24
comjun04 <30339539+comjun04@users.noreply.github.com>
2020-01-07 11:16:11 +0900

    brand new Database Provider

--- db/DatabaseProvider.js
+++ db/DatabaseProvider.js
@@ -1,7 +1,24 @@
 const { SettingProvider } = require('discord.js-commando')
 
 class DatabaseProvider extends SettingProvider {
+  /*
+   * Get the value from the database.
+   * @param {Object} tableOrRootNode `table` if the database is relational (like mysql, postgres), or `rootNode` if NoSQL (like json, RethinkDB)
+   * @param {string|number} the id of user or guild
+   * @param {Array<string>} columnsOrProperties `columns` if relational database (like mysql, postgres), or `properties` if NoSQL (like json, RethinkDB)
+   * @returns {Object} the value with the key which is the element of `columnsOrProperties`
+   * @abstract
+   */
+  async get(tableOrRootNode, id, columnsOrProperties) {
+    throw new Error('get() is not implemented')
+  }
 
+  /*
+   * Set the value to the database.
+   */
+  async set(tableOrRootNode, id, data) {
+    throw new Error('set() is not implemented')
+  }
 }
 
 module.exports = DatabaseProvider
