1400cc921f9ca4e64ffeca0327489c7d94e2e30d
comjun04 <30339539+comjun04@users.noreply.github.com>
2020-01-09 13:28:31 +0900

    DatabaseProvider: added other template

--- db/DatabaseProvider.js
+++ db/DatabaseProvider.js
@@ -2,6 +2,21 @@ const { SettingProvider } = require('discord.js-commando')
 
 class DatabaseProvider extends SettingProvider {
   /*
+   * Removes all settings in a guild or user
+   * @param {string} type the type of id to clear the settings. Only accepts `'guild'` or `'user'`.
+   * @param {string} id the id of guild or user.
+   */
+  async clear(type, id) {
+    throw new Error('clear() is not implemented')
+  }
+
+  /*
+  async destroy() {
+    throw new Error('destroy() is not implemented')
+  }
+  */
+
+  /*
    * Get the value from the database.
    * @param {Object} tableOrRootNode `table` if the database is relational (like mysql, postgres), or `rootNode` if NoSQL (like json, RethinkDB)
    * @param {string|number} the id of user or guild
@@ -11,6 +26,17 @@ class DatabaseProvider extends SettingProvider {
    */
   async get(tableOrRootNode, id, columnsOrProperties) {
     throw new Error('get() is not implemented')
+  }
+
+  /*
+   * Removes a value from the database.
+   * @param {Object} tableOrRootNode `table` if the database is relational (like mysql, postgres), or `rootNode` if NoSQL (like json, RethinkDB)
+   * @param {string|number} the id of user or guild
+   * @param {Array<string>} columnsOrProperties `columns` if relational database (like mysql, postgres), or `properties` if NoSQL (like json, RethinkDB)
+   * @returns {Promise<Object>} the Object with the previous value, mapped with their columns or properties' name.
+   */
+  async remove(tableOrRootNode, id, columnsOrProperties) {
+    throw new Error('remove() is not implemented')
   }
 
   /*
