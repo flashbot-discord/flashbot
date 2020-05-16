be4c6fafc80fa37611ff98c9a23dce2459af4eb3
comjun04 <comjun04@gmail.com>
2020-05-16 15:04:25 +0900

    commands/say: blocked saying @everyone and @here

--- commands/misc/say.js
+++ commands/misc/say.js
@@ -3,6 +3,7 @@
  * @description 사용자가 입력한 말을 따라 말하는 명령어입니다.
  */
 
+const { MessageMentions } = require('discord.js')
 const Command = require('../../classes/Command')
 
 class SayCommand extends Command {
@@ -23,7 +24,14 @@ class SayCommand extends Command {
   }
 
   async run (client, msg, query, locale) {
+    const t = client.locale.t
     if (query.args.length < 1) return await msg.reply(Command.makeUsage(this, query.cmd, locale))
+
+    const str = query.args.join(' ')
+
+    // Reset the pattern count
+    MessageMentions.EVERYONE_PATTERN.lastIndex = 0
+    if(MessageMentions.EVERYONE_PATTERN.test(str)) return await msg.reply(t('commands.say.noEveryone', locale))
     return await msg.channel.send(query.args.join(' '))
   }
 }
