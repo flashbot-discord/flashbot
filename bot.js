1cbe34c3a21aeea8fd0bf9fe9899468bf5625b9c
comjun04 <comjun04@gmail.com>
2020-05-29 00:15:55 +0900

    v0.8-beta.2

--- bot.js
+++ bot.js
@@ -4,8 +4,8 @@
  */
 
 /* eslint-disable no-unused-vars */
-const VERSION = 'v0.8-beta.1'
-const BUILD_DATE = '2020/5/25'
+const VERSION = 'v0.8-beta.2'
+const BUILD_DATE = '2020/5/29'
 /* eslint-enable no-unused-vars */
 
 // import needed modules
