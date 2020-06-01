19aaac6b185b8db3698c685ec6e7f558e2c9bf39
comjun04 <comjun04@gmail.com>
2020-06-01 10:43:45 +0900

    command/locale: added multi-perms support

--- commands/misc/locale.js
+++ commands/misc/locale.js
@@ -26,19 +26,24 @@ class LocaleCommand extends Command {
   }
 
   async run (client, msg, query, locale) {
+    const t = client.locale.t
+    const editPerms = ['ADMINISTRATOR']
+
     if (!await client.db.isRegisteredGuild(msg.guild.id)) return Command.pleaseRegisterGuild(this, msg, locale)
 
-    if (query.args.length < 1) {
-      return await msg.channel.send(client.locale.t('commands.locale.get:The current language is: `%1$s`', locale, locale))
-    } else {
-      if (!client.config.owner.includes(msg.author.id) && !msg.member.permissions.has('ADMINISTRATOR')) return await msg.channel.send(client.locale.t("commands.locale.noPermission:You don't have `ADMINISTRATOR` permission to do that.", locale))
-      else {
-        const language = query.args[0]
-        if (!client.locale.i18n.getLocales().includes(language)) return await msg.channel.send(client.locale.t('commands.locale.noLanguage:The language you entered does not exist.', locale))
-        await this.dbHandle(msg.guild, language)
-        await msg.channel.send(client.locale.t('commands.locale.set:Language setting changed to `%1$s`.', locale, language))
-      }
+    if (query.args.length < 1) return await msg.channel.send(t('commands.locale.get', locale, locale))
+
+    if (!client.config.owner.includes(msg.author.id) && !msg.member.permissions.any(editPerms)) {
+      let translatedPerms = []
+      editPerms.forEach((p) => translatedPerms.push(t('perms.' + p, locale)))
+      return await msg.channel.send(t("commands.locale.noPermission", locale, translatedPerms.join('`, `')))
     }
+
+    const language = query.args[0]
+    if (!client.locale.i18n.getLocales().includes(language)) return await msg.channel.send(client.locale.t('commands.locale.noLanguage:The language you entered does not exist.', locale))
+
+    await this.dbHandle(msg.guild, language)
+    await msg.channel.send(client.locale.t('commands.locale.set:Language setting changed to `%1$s`.', locale, language))
   }
 
   async dbHandle (guild, locale) {
