f7d9b46bdd44d63fd7748bddf9546514d0e8eba5
comjun04 <comjun04@gmail.com>
2020-07-31 16:45:31 +0900

    Make logger display formatted time and in ms

--- classes/Logger.js
+++ classes/Logger.js
@@ -29,11 +29,12 @@ class Logger {
   _genDatetime () {
     const date = new Date()
     return '[' + date.getFullYear() + '-' +
-      (date.getMonth() + 1) + '-' +
-      date.getDate() + ' ' +
-      date.getHours() + ':' +
-      date.getMinutes() + ':' +
-      date.getSeconds() + ']'
+      String(date.getMonth() + 1).padStart(2, '0') + '-' +
+      String(date.getDate()).padStart(2, '0') + ' ' +
+      String(date.getHours()).padStart(2, '0') + ':' +
+      String(date.getMinutes()).padStart(2, '0') + ':' +
+      String(date.getSeconds()).padStart(2, '0') + '.' +
+      String(date.getMilliseconds()).padStart(3, '0') + ']'
   }
 }
 
