83c85e74cdcece23b9db5efda13798d459a01c1e
comjun04 <comjun04@gmail.com>
2020-08-18 12:10:43 +0900

    feat(typing): typing game
    타자 게임 추가
    
    Squashed commit of the following:
    
    commit ec1e31e48dc70d09d5979bc4176c5bf8e512854b
    Author: comjun04 <comjun04@gmail.com>
    Date:   Tue Aug 18 12:06:03 2020 +0900
    
        chore: eslint fix
    
    commit a8db47b79c4d9a4abb3429f1528e20c10c5b3209
    Author: comjun04 <comjun04@gmail.com>
    Date:   Tue Aug 18 12:03:10 2020 +0900
    
        fix(typing): change data file location
    
        데이터 파일 위치 수정
    
    commit 18ba852f9dcfeee787b08c75b41f619051a0f197
    Author: comjun04 <comjun04@gmail.com>
    Date:   Tue Aug 18 11:48:08 2020 +0900
    
        Removed submodule data/typing
    
    commit 9b1600f3e0a0bb6f42a9041d14fd4790287d517f
    Author: comjun04 <comjun04@gmail.com>
    Date:   Tue Aug 11 23:36:06 2020 +0900
    
        update english content
    
    commit 922704c57888ffc9c91469e611cd4fc43ec5fbed
    Author: comjun04 <comjun04@gmail.com>
    Date:   Wed Jul 15 13:07:39 2020 +0900
    
        typing: add stop aliases
    
    commit 5119fe575ab57d81b7e57b06230726e913ab68ed
    Author: comjun04 <comjun04@gmail.com>
    Date:   Wed Jul 8 16:52:32 2020 +0900
    
        typing: add english mode
    
    commit 1b21092b55ee0e5ac69f419c6707bfd76e17dc09
    Author: comjun04 <comjun04@gmail.com>
    Date:   Mon Jul 6 01:13:06 2020 +0900
    
        improve typing rate per minute when including korean
    
    commit d195c79a607cd91ac433eed5c062548a39d882ea
    Author: comjun04 <comjun04@gmail.com>
    Date:   Mon Jun 22 23:58:41 2020 +0900
    
        missing category patch
    
    commit b9e713bbfc938acae73abfb7c4bcd0d074292aca
    Author: comjun04 <comjun04@gmail.com>
    Date:   Mon Jun 22 23:43:00 2020 +0900
    
        add typing game

---
+++ commands/game/typing.js
@@ -0,0 +1,140 @@
+const fs = require('fs')
+const path = require('path')
+const { Collection, MessageCollector } = require('discord.js')
+const hangul = require('hangul-js')
+
+const Command = require('../../classes/Command')
+
+class TypingGameCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'typing',
+      aliases: ['타자'],
+      description: 'commands.typing.DESC',
+      group: 'game',
+      guildAct: true
+    })
+
+    this.loaded = false
+    this.loading = false
+    this.default = 'ko_KR'
+    this.session = new Collection()
+    this.data = new Collection()
+    this.path = path.join(path.resolve(), 'modules', 'typing')
+  }
+
+  async run (client, msg, query, locale) {
+    const t = client.locale.t
+
+    switch (query.args[0]) {
+      case 'reload':
+      case '리로드':
+        this.loaded = false
+        this.data.forEach((_, lang) => {
+          delete require.cache[path.join(this.path, lang + '.json')]
+        })
+
+        return this.loadData(msg, locale)
+
+      case 'start':
+      case '시작': {
+        if (!this.loaded) {
+          msg.channel.send(t('commands.typing.loading', locale))
+          if (this.loading) return
+          this.loadData(msg, locale)
+        }
+
+        // Stop when session is present
+        if (this.session.has(msg.channel.id)) return msg.channel.send(t('commands.typing.alreadyPlaying', locale))
+
+        // Session placeholder
+        this.session.set(msg.channel.id, {})
+
+        // Choose Language
+        let lang = this.default
+        if (query.args[1]) {
+          if (['영어', 'english'].includes(query.args[1])) lang = 'en_US'
+        }
+
+        if (!this.data.get(this.default)) {
+          msg.channel.send(t('commands.typing.noDefaultData'))
+          return this.stop(msg, locale)
+        }
+        const langData = this.data.get(lang)
+        if (!langData) {
+          msg.channel.send(t('commands.typing.langDataNotExist', locale))
+          return this.stop(msg, locale)
+        }
+
+        const text = langData[Math.floor(Math.random() * langData.length)]
+        const displayText = text.split('').join('\u200b')
+
+        await msg.channel.send(t('commands.typing.start', locale, displayText))
+
+        // Timer start
+        const startTime = Date.now()
+
+        const mc = msg.channel.createMessageCollector((m) => !m.author.bot, { time: 60000 })
+
+        // push the collector to the session storage
+        this.session.set(msg.channel.id, mc)
+
+        mc.on('collect', (m) => {
+          if (m.content === displayText) return msg.channel.send('<@' + m.author.id + '>, ' + t('commands.typing.doNotCopyPaste', locale))
+
+          if (m.content !== text) return // msg.channel.send('<@' + m.author.id + '>, ' + t('commands.typing.notMatch', locale))
+
+          const time = (Date.now() - startTime) / 1000
+          const ta = Math.round(hangul.d(text).length / time * 60)
+          msg.channel.send('<@' + m.author.id + '>, ' + t('commands.typing.correct', locale, time, ta))
+          mc.stop('correct')
+        })
+
+        mc.on('end', (_, reason) => {
+          // if (reason === 'correct') return
+          if (reason === 'stopcmd') msg.channel.send(t('commands.typing.cmdStop', locale))
+          else if (reason !== 'correct') msg.channel.send(t('commands.typing.finish', locale))
+
+          // remove channel from session storage
+          this.session.delete(msg.channel.id)
+        })
+
+        break
+      }
+
+      case 'stop':
+      case '종료':
+      case '정지':
+      case '중지':
+        this.stop(msg, locale)
+    }
+  }
+
+  stop (msg, locale) {
+    if (!this.session.has(msg.channel.id)) return msg.channel.send(msg.client.locale.t('commands.typing.notPlaying', locale))
+
+    if (this.session.get(msg.channel.id) instanceof MessageCollector) this.session.get(msg.channel.id).stop('stopcmd')
+    this.session.delete(msg.channel.id)
+  }
+
+  loadData (msg, locale) {
+    // TODO make it async
+    const t = msg.client.locale.t
+
+    if (!fs.existsSync(this.path)) return msg.channel.send(t('commands.typing.noDataFolder', locale))
+
+    const locales = fs.readdirSync(this.path)
+    if (locales.length < 1) return msg.channel.send(t('commands.typing.noDataFile', locale))
+
+    locales.forEach((l) => {
+      if (!fs.lstatSync(path.join(this.path, l)).isFile() || !l.endsWith('.json')) return
+      this.data.set(l.slice(0, -5), require(path.join(this.path, l)))
+    })
+
+    this.loaded = true
+    this.loading = false
+    msg.channel.send(t('commands.typing.loaded', locale))
+  }
+}
+
+module.exports = TypingGameCommand
