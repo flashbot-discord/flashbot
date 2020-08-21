a048870c04ff83a9922064589b784cf0bb5f4aee
comjun04 <comjun04@gmail.com>
2020-08-21 22:25:59 +0900

    Fix prefix getting system when database is not ready

--- modules/getPrefix.js
+++ modules/getPrefix.js
@@ -1,7 +1,8 @@
 const database = require('../database')
 
 module.exports = async (client, guildid) => {
-  if (!guildid) return ''
+  if (!client.db.ready) return client.config.prefix
+  else if (!guildid) return ''
 
   const guildPrefix = await database.guilds.prefix.get(client.db, guildid)
   return guildPrefix == null ? client.config.prefix : guildPrefix
