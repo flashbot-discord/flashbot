912ebda2f1fbef7c76bf2a0015b0ddd65710987f
comjun04 <comjun04@gmail.com>
2020-08-24 20:20:02 +0900

    Add new typing data structure and category system

--- commands/game/typing.js
+++ commands/game/typing.js
@@ -11,8 +11,7 @@ class TypingGameCommand extends Command {
       name: 'typing',
       aliases: ['타자'],
       description: 'commands.typing.DESC',
-      group: 'game',
-      guildAct: true
+      group: 'game'
     })
 
     this.loaded = false
@@ -20,7 +19,9 @@ class TypingGameCommand extends Command {
     this.default = 'ko_KR'
     this.session = new Collection()
     this.data = new Collection()
-    this.path = path.join(path.resolve(), 'modules', 'typing')
+    this.locales = new Collection()
+    this.path = path.join(path.resolve(), 'data', 'typing')
+    this._logPos = 'Command / typing'
   }
 
   async run (client, msg, query, locale) {
@@ -55,20 +56,21 @@ class TypingGameCommand extends Command {
         // Choose Language
         let lang = this.default
         if (query.args[1]) {
-          if (['영어', 'english'].includes(query.args[1])) lang = 'en_US'
+          const l = this.locales.get(query.args[1])
+          if (this.data.has(l)) lang = l
         }
 
-        if (!this.data.get(this.default)) {
-          msg.channel.send(t('commands.typing.noDefaultData'))
-          return this.stop(msg, locale)
-        }
         const langData = this.data.get(lang)
         if (!langData) {
           msg.channel.send(t('commands.typing.langDataNotExist', locale))
           return this.stop(msg, locale)
         }
 
-        const text = langData[Math.floor(Math.random() * langData.length)]
+        // TODO category select
+        const category = langData.random()
+        const data = category.data[Math.floor(Math.random() * category.data.length)]
+
+        const { text } = data
         const displayText = text.split('').join('\u200b')
 
         await msg.channel.send(t('commands.typing.start', locale, displayText))
@@ -122,15 +124,55 @@ class TypingGameCommand extends Command {
   loadData (msg, locale) {
     // TODO make it async
     const t = msg.client.locale.t
+    this.loading = true
+    const logPos = this._logPos + '.loadData'
+
+    msg.client.logger.log(logPos, 'Start loading typing data...')
 
     if (!fs.existsSync(this.path)) return msg.channel.send(t('commands.typing.noDataFolder', locale))
 
     const locales = fs.readdirSync(this.path)
-    if (locales.length < 1) return msg.channel.send(t('commands.typing.noDataFile', locale))
+    if (locales.length < 1) return msg.channel.send(t('commands.typing.noLocaleFolder', locale))
 
     locales.forEach((l) => {
-      if (!fs.lstatSync(path.join(this.path, l)).isFile() || !l.endsWith('.json')) return
-      this.data.set(l.slice(0, -5), require(path.join(this.path, l)))
+      msg.client.logger.log(logPos, `Loading typing data for locale '${l}'...`)
+      msg.client.logger.debug(logPos, 'load path: ' + path.join(this.path, l))
+      if (
+        !(fs.lstatSync(path.join(this.path, l)).isDirectory()) ||
+        !fs.existsSync(path.join(this.path, l, 'manifest.json')) ||
+        !(fs.lstatSync(path.join(this.path, l, 'manifest.json')).isFile())
+      ) return
+      
+      const manifest = JSON.parse(fs.readFileSync(path.join(this.path, l, 'manifest.json')).toString())
+      const data = new Collection()
+      const tempDataSortedByGroup = {}
+
+      manifest.groups.forEach((group) => {
+        msg.client.logger.debug(logPos, `register group '${group.id}'`)
+        data.set(group.id, group)
+        tempDataSortedByGroup[group.id] = []
+      })
+      manifest.locale.forEach((ll) => {
+        msg.client.logger.debug(logPos, `register locale alias '${ll}'`)
+        this.locales.set(ll, l)
+      })
+
+      manifest.files.forEach((file) => {
+        msg.client.logger.debug(logPos, `load data file '${file}'`)
+        const textData = JSON.parse(fs.readFileSync(path.join(this.path, l, file)).toString())
+        textData.forEach((td) => {
+          tempDataSortedByGroup[td.group].push(td)
+        })
+      })
+
+      manifest.groups.forEach((group) => {
+        msg.client.logger.debug(logPos, `applying data file to group '${group.id}'`)
+        const tempData = data.get(group.id)
+        tempData.data = tempDataSortedByGroup[group.id]
+        data.set(group.id, tempData)
+      })
+
+      this.data.set(l, data)
     })
 
     this.loaded = true
