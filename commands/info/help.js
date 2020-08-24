89c6f7a774f86f7d1011d1dddb41f658f7dcd4de
comjun04 <comjun04@gmail.com>
2020-08-24 11:57:19 +0900

    Fix total page number in first help embed

--- commands/info/help.js
+++ commands/info/help.js
@@ -67,7 +67,7 @@ class HelpCommand extends Command {
       return embed
     }
 
-    embeds.push(createEmbed('info', 1, 3, true))
+    embeds.push(createEmbed('info', 1, 5, true))
     embeds.push(createEmbed('activation', 2, 5, page))
     embeds.push(createEmbed('util', 3, 5, page))
     embeds.push(createEmbed('settings', 4, 5, page))
