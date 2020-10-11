c5c97047cf877b83642ebbd9bb65ec62ff32e6ce
comjun04 <comjun04@gmail.com>
2020-10-11 11:59:15 +0900

    feat(cmd/notice): Add notice command

---
+++ commands/dev/notice.js
@@ -0,0 +1,56 @@
+const { MessageEmbed } = require('discord.js')
+
+const Command = require('../../classes/Command')
+
+class NoticeCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'notice',
+      aliases: ['공지', 'ㅜㅐ샻ㄷ', 'rhdwl'],
+      description: 'commands.notice.DESC',
+      group: 'dev',
+      owner: true,
+      clientPerms: ['EMBED_LINKS', 'ADD_REACTIONS']
+    })
+  }
+
+  async run (client, msg, query, locale) {
+    const t = client.locale.t
+    const text = query.args.join(' ')
+
+    const embed = new MessageEmbed()
+      .setTitle(t('commands.notice.previewEmbed.title', locale))
+      .setDescription(text)
+
+    const botMsg = await msg.reply(t('commands.notice.confirm', locale), embed)
+
+    botMsg.react('✅')
+    botMsg.react('❌')
+
+    const collected = await botMsg.awaitReactions((reaction, user) => {
+      if (user.id !== msg.author.id) return false
+      else return ['✅', '❌'].includes(reaction.emoji.name)
+    }, {
+      max: 1,
+      time: 1000 * 30
+    })
+
+    if (collected.size < 1) {
+      // Timeout
+      return msg.reply(t('commands.notice.timeOut', locale))
+    } else {
+      const emoji = collected.first().emoji.name
+      if (emoji === '❌') {
+        // User cancelled
+        return msg.reply(t('commands.notice.cancelled', locale))
+      } else if (emoji === '✅') {
+        // Run
+        // TODO sendNotice module here
+
+        msg.reply(t('commands.notice.started', locale))
+      }
+    }
+  }
+}
+
+module.exports = NoticeCommand
