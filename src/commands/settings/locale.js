8262c5d7c19e5a57ea0075ec63657bdd17211626
comjun04 <comjun04@gmail.com>
2020-12-08 01:45:52 +0900

    fix(locale): Modify locale command to fit for new args system

--- src/commands/settings/locale.js
+++ src/commands/settings/locale.js
@@ -18,37 +18,50 @@ class LocaleCommand extends Command {
       group: 'settings',
       requireDB: true,
       userReg: true,
-      args: [
-        {
-          key: 'locale',
-          description: 'commands.locale.args.locale.DESC: When present, change the locale of the server to this value.',
-          type: 'string',
-          optional: true
-        }
-      ]
+      args: {
+        guild: {
+          description: 'whether the locale shuold be set to guild. Only works on set mode.',
+          aliases: ['g'],
+          type: 'boolean'
+        },
+        _: [
+          {
+            key: 'mode',
+            description: 'commands.locale.args.mode.DESC: When present, change the locale of the server to this value.',
+            type: 'string',
+            optional: true
+          },
+          {
+            key: 'locale',
+            description: 'commands.locale.args.locale.DESC:The locale to change. Only works in set mode.',
+            type: 'string',
+            optional: true
+          }
+        ]
+      }
     })
   }
 
   async run (client, msg, query, { t }) {
     const editPerms = ['MANAGE_GUILD']
 
-    if (query.args.length < 1) {
+    if (!query.args.mode) {
       const guildLocale = await client.locale.getLocale(true, msg.guild)
       return msg.channel.send(t('commands.locale.get', guildLocale))
     }
 
-    const subcommand = query.args[0]
+    const subcommand = query.args.mode
     switch (subcommand) {
       case 'list':
         return msg.channel.send(t('commands.locale.list', client.locale.i18n.getLocales().join('`\n`')))
 
       case 'set': {
-        const language = query.args[1]
+        const language = query.args.locale
         if (!client.locale.i18n.getLocales().includes(language)) return msg.channel.send(t('commands.locale.noLanguage'))
 
         let id = msg.author.id
         let guildMode = false
-        if (['--guild', '-g'].includes(query.args[2])) {
+        if (query.args.guild) {
           if (!client.config.owner.includes(msg.author.id) && !msg.member.permissions.any(editPerms)) {
             const translatedPerms = []
             editPerms.forEach((p) => translatedPerms.push(t('perms.' + p)))
