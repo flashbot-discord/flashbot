fd938336f7dd46ab5fe42ca50a8f3d8de71eee17
comjun04 <comjun04@gmail.com>
2020-12-26 01:39:37 +0900

    feat(cmd/reload): Add reloading all commands

--- src/commands/dev/reload.js
+++ src/commands/dev/reload.js
@@ -4,22 +4,50 @@ class ReloadCommand extends Command {
   constructor (client) {
     super(client, {
       name: 'reload',
-      aliases: ['ㄱ디ㅐㅁㅇ'],
+      aliases: ['리로드', 'ㄱ디ㅐㅁㅇ', 'flfhem'],
       description: 'commands.reload.DESC:reloads a command',
       group: 'dev',
       owner: true,
-      args: [
-        {
-          key: 'command',
-          type: 'string',
-          description: 'commands.reload.args.command.DESC:Command to reload'
-        }
-      ]
+      args: {
+        all: {
+          aliases: ['a', '모두'],
+          type: 'boolean',
+          description: '',
+          optional: true
+        },
+        _: [
+          {
+            key: 'command',
+            type: 'string',
+            description: 'commands.reload.args.command.DESC:Command to reload'
+          }
+        ]
+      }
     })
   }
 
   async run (client, msg, query, { t }) {
     if (query.args.length < 1) return msg.reply(Command.makeUsage(this, query.cmd))
+
+    if (query.args.all) {
+      const list = client.commands.commands.clone()
+      const errCmds = []
+      list.forEach(cmd => {
+        try {
+          cmd.reload()
+        } catch (e) {
+          errCmds.push(cmd._name)
+        }
+      })
+
+      const successCmdCount = list.size - errCmds.length
+
+      // TODO: English plural text support
+      let result = t('commands.reload.allReload.result', list.size, successCmdCount)
+      if (errCmds.length > 0) result += '\n' + t('commands.reload.allReload.failedCmds', errCmds.length, errCmds.join('`, `'))
+
+      return msg.reply(result)
+    }
 
     const input = query.args.command
     const cmd = client.commands.get(input)
