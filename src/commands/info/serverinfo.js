b52a059f73b153a3ccdb975321e32f9c18364d7b
My Name <comjun04@gmail.com>
2021-01-25 01:02:09 +0900

    feat(cmd/serverinfo): Add server boost status

--- src/commands/info/serverinfo.js
+++ src/commands/info/serverinfo.js
@@ -27,27 +27,45 @@ class ServerInfoCommand extends Command {
     const [bots, users] = members.partition((member) => member.user.bot)
     const userCount = users.size
     const botCount = bots.size
-    const memberCountText = `:adult: ${tn('commands.serverinfo.memberCount.value.userCount', userCount)}\n` +
-      `:robot: ${tn('commands.serverinfo.memberCount.value.botCount', botCount)}`
+    const memberCountText = `
+:adult: ${tn('commands.serverinfo.memberCount.value.userCount', userCount)}
+:robot: ${tn('commands.serverinfo.memberCount.value.botCount', botCount)}`
 
     const channelCache = guild.channels.cache
     const textChannelCount = channelCache.filter((c) => c.type === 'text').size
     const voiceChannelCount = channelCache.filter((c) => c.type === 'voice').size
     const categoryCount = channelCache.filter((c) => c.type === 'category').size
     const newsChannelCount = channelCache.filter((c) => c.type === 'news').size
-    const channelCountText = `:keyboard: ${tn('commands.serverinfo.channelCount.value.textChannelCount', textChannelCount)}\n` +
-      `:microphone2: ${tn('commands.serverinfo.channelCount.value.voieChannelCount', voiceChannelCount)}\n` +
-      `:file_folder: ${tn('commands.serverinfo.channelCount.value.categoryCount', categoryCount)}\n` +
-      `:newspaper: ${tn('commands.serverinfo.channelCount.value.newsChannelCount', newsChannelCount)}`
+    const channelCountText = `
+:keyboard: ${tn('commands.serverinfo.channelCount.value.textChannelCount', textChannelCount)}
+:microphone2: ${tn('commands.serverinfo.channelCount.value.voieChannelCount', voiceChannelCount)}
+:file_folder: ${tn('commands.serverinfo.channelCount.value.categoryCount', categoryCount)}
+:newspaper: ${tn('commands.serverinfo.channelCount.value.newsChannelCount', newsChannelCount)}`
 
     const verificationLevel = guild.verificationLevel
-    const verificationLevelText = `**${t(`verificationLevel.${verificationLevel}.name`)}** (\`${verificationLevel}\`)\n` +
-      t(`verificationLevel.${verificationLevel}.description`)
+    const verificationLevelText = `
+**${t(`verificationLevel.${verificationLevel}.name`)}** (\`${verificationLevel}\`)
+${t(`verificationLevel.${verificationLevel}.description`)}`
 
     const is2FAReqOn = guild.mfaLevel === 1
     const twoFARequireForModText = `${is2FAReqOn ? ':white_check_mark:' : ':x:'} ${t(`commands.serverinfo.2faRequireForMod.value.${is2FAReqOn ? 'enabled' : 'disabled'}`)} (\`${guild.mfaLevel}\`)`
 
-    const createdAt = moment(guild.createdAt).tz('Asia/Seoul').format(t('commands.serverinfo.createdAt.value'))
+    const createdAt = moment(guild.createdAt)
+      .tz('Asia/Seoul')
+      .format(t('commands.serverinfo.createdAt.value'))
+
+    const serverBoostLevelArr = [2, 15, 30, null]
+    const currentServerBoost = guild.premiumSubscriptionCount
+    const requiredBoostForNextLevel = serverBoostLevelArr[guild.premiumTier]
+    const serverBoostText = `${t('commands.serverinfo.serverBoost.content.level', {
+      level: guild.premiumTier
+    })} | ${guild.premiumTier >= 3
+        ? t('commands.serverinfo.serverBoost.content.maxLevel')
+        : t('commands.serverinfo.serverBoost.content.forNextLevel', {
+          requiredBoost: requiredBoostForNextLevel,
+          currentBoost: currentServerBoost,
+          boostForNextLevel: requiredBoostForNextLevel
+        })}`
 
     if (useEmbed) {
       data = new MessageEmbed()
@@ -63,18 +81,28 @@ class ServerInfoCommand extends Command {
         .addField(':busts_in_silhouette: ' + tn('commands.serverinfo.memberCount.title', guild.memberCount), memberCountText, true)
         .addField(':tv: ' + tn('commands.serverinfo.channelCount.title', guild.channels.cache.size), channelCountText, true)
         .addField(':birthday: ' + t('commands.serverinfo.createdAt.title'), createdAt)
+        .addField(':diamonds: ' + t('commands.serverinfo.serverBoost.title', {
+          boost: currentServerBoost
+        }), serverBoostText)
     } else {
-      data = `**${t('commands.serverinfo.title', guild.name)}**\n` +
-        `(${t('commands.serverinfo.requestedBy', `<@${msg.author.id}>`, msg.author.tag)})\n\n` +
-        `**:desktop: ${t('commands.serverinfo.name')}**: ${guild.name}\n` +
-        `**:id: ${t('commands.serverinfo.id')}**: ${guild.id}\n` +
-        `**:crown: ${t('commands.serverinfo.owner')}**: ${guild.owner.user.tag} (${guild.owner.id})\n` +
-        `**:map: ${t('commands.serverinfo.region')}**: ${t('regions.' + guild.region)} (\`${guild.region}\`)\n` +
-        `**:shield: ${t('commands.serverinfo.verificationLevel')}**: ${verificationLevelText}\n` +
-        `**:lock: ${t('commands.serverinfo.2faRequireForMod.title')}**: ${twoFARequireForModText}\n\n` +
-        `**:busts_in_silhouette: ${tn('commands.serverinfo.memberCount.title', guild.memberCount)}**\n${memberCountText}\n\n` +
-        `**:tv: ${tn('commands.serverinfo.channelCount.title', guild.channels.cache.size)}**\n${channelCountText}\n\n` +
-        `**:birthday: ${t('commands.serverinfo.createdAt.title')}**: ${createdAt}`
+      data = `
+**${t('commands.serverinfo.title', guild.name)}**
+(${t('commands.serverinfo.requestedBy', `<@${msg.author.id}>`, msg.author.tag)})
+
+**:desktop: ${t('commands.serverinfo.name')}**: ${guild.name}
+**:id: ${t('commands.serverinfo.id')}**: ${guild.id}
+**:crown: ${t('commands.serverinfo.owner')}**: ${guild.owner.user.tag} (${guild.owner.id})
+**:map: ${t('commands.serverinfo.region')}**: ${t('regions.' + guild.region)} (\`${guild.region}\`)
+**:shield: ${t('commands.serverinfo.verificationLevel')}**: ${verificationLevelText}
+**:lock: ${t('commands.serverinfo.2faRequireForMod.title')}**: ${twoFARequireForModText}
+
+**:busts_in_silhouette: ${tn('commands.serverinfo.memberCount.title', guild.memberCount)}**
+${memberCountText}
+
+**:tv: ${tn('commands.serverinfo.channelCount.title', guild.channels.cache.size)}**
+${channelCountText}
+
+**:birthday: ${t('commands.serverinfo.createdAt.title')}**: ${createdAt}`
     }
 
     return msg.channel.send(data)
