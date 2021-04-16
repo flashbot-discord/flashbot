45acc8d2d7e0a076db0ff755ac4c99bdb1d2b9c1
My Name <comjun04@gmail.com>
2021-04-16 16:18:10 +0900

    feat(cmd): base64 command

---
+++ src/commands/util/base64.js
@@ -0,0 +1,40 @@
+const Command = require('../_Command')
+
+class Base64Command extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'base64',
+      aliases: ['b64'],
+      group: 'util',
+      args: [
+        {
+          key: 'cmd',
+          type: 'string'
+        },
+        {
+          key: 'value',
+          type: 'text'
+        }
+      ]
+    })
+  }
+
+  async run (client, msg, query, { t }) {
+    switch (query.args.cmd) {
+      case 'e':
+      case 'encode':
+        msg.channel.send(Buffer.from(query.args.value).toString('base64'))
+        break
+
+      case 'd':
+      case 'decode':
+        msg.channel.send(Buffer.from(query.args.value, 'base64').toString())
+          .catch(e => {
+            msg.react('⚠️')
+            msg.reply(t('commands.base64.emptyMsg'))
+          })
+    }
+  }
+}
+
+module.exports = Base64Command
