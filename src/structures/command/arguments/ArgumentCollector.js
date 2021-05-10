6e94ed782b58b9d44d987732c8a1bdaff0581282
comjun04 <comjun04@gmail.com>
2021-05-10 16:56:21 +0900

    fix: temp

--- src/structures/command/arguments/ArgumentCollector.js
+++ src/structures/command/arguments/ArgumentCollector.js
@@ -5,16 +5,17 @@ const logger = loggerGen('ArgumentCollector')
 class ArgumentCollector {
   constructor (parentCommand) {
     this._command = parentCommand
-    this.args = { named: {}, unnamed: [] }
+    this.args = []
+    this.flags = {}
     this.dynamic = false
   }
 
   /**
-   * Register named argument
-   * @param {string} argName name of the argument
-   * @param {*} argInfo information object of the argument
+   * Register a flag
+   * @param {string} flagName name of the flag
+   * @param {*} flagInfo information object of the flag
    */
-  registerNamedArgument (argName, argInfo) {
+  registerFlag (flagName, flagInfo) {
     /*
     name: string (throws Error if not provided): {
       aliases: Array<string> ([]),
@@ -23,22 +24,61 @@ class ArgumentCollector {
       default: any (undefined)
     }
     */
-    logger.debug('argName = %s', argName)
-    logger.debug('argInfo = %O', argInfo)
+    logger.debug('flagName = %s', flagName)
+    logger.debug('flagInfo = %O', flagInfo)
 
     // name
-    if (argName in this.args.named) throw new Error('Argument name already exists')
+    if (flagName in this.flags) throw new Error('Flag already exists')
 
     // aliases
     const aliases = []
-    if (Array.isArray(argInfo.aliases)) {
-      for (const alias of argInfo.aliases) {
+    if (Array.isArray(flagInfo.aliases)) {
+      for (const alias of flagInfo.aliases) {
         if (!aliases.includes(alias)) aliases.push(alias)
       }
-    } else if (argInfo.aliases != null) throw new TypeError('Argument aliases must be Array<string> or null')
+    } else if (flagInfo.aliases != null) throw new TypeError('Argument aliases must be Array<string> or null')
 
     // type
-    if (!this._validateType(argInfo.type)) argInfo.type = null
+    if (!this._validateType(flagInfo.type)) flagInfo.type = null
+
+    // NOTE: description
+    // if empty, return null to use locale parsing at execution
+    const description = typeof flagInfo.description === 'string' && flagInfo.description.length > 0
+      ? flagInfo.description
+      : null
+
+    // optional
+    const optional = Boolean(flagInfo.optional)
+
+    // register flag to registry
+    this.flags[flagName] = {
+      aliases,
+      type: flagInfo.type,
+      description,
+      optional,
+      default: flagInfo.default
+    }
+  }
+
+  /**
+   * Register an argument.
+   * @param {*} argInfo
+   */
+  registerArgument (argInfo) {
+    logger.debug('argInfo = %O', argInfo)
+
+    // name
+    const key = argInfo.key
+    if (typeof key !== 'string' || key.length < 1) {
+      throw new Error('Argument key is empty')
+    } else if (this.args.some(a => a.key === key)) {
+      throw new Error('Argument name already exists')
+    }
+
+    // type
+    if (!this._validateType(argInfo.type)) {
+      argInfo.type = null
+    }
 
     // NOTE: description
     // if empty, return null to use locale parsing at execution
@@ -46,24 +86,27 @@ class ArgumentCollector {
       ? argInfo.description
       : null
 
-    // optional
+    // optional field
     const optional = Boolean(argInfo.optional)
 
-    // register arg to registry
-    this.args.named[argName] = {
-      aliases,
+    // infinity field
+    const infinity = Boolean(argInfo.infinity)
+
+    this.args.push({
+      key,
       type: argInfo.type,
       description,
       optional,
-      default: argInfo.default
-    }
+      default: argInfo.default,
+      infinity
+    })
   }
 
   /**
-   * Register unnamed arguments
+   * Register arguments
    * @param {Array} argInfosArr
    */
-  registerUnnamedArguments (argInfosArr) {
+  registerArguments (argInfosArr) {
     /*
     {
       key: string (throws Error if not provided),
@@ -73,40 +116,11 @@ class ArgumentCollector {
       infinity: boolean (false)
     }
     */
-    logger.verbose('registering unnamed args')
-    logger.debug('argInfosArr = %O', argInfosArr)
+    logger.verbose('registering args')
+    // logger.debug('argInfosArr = %O', argInfosArr)
 
     for (const argInfo of argInfosArr) {
-      logger.debug('argInfo = %O', argInfo)
-
-      // name
-      const key = argInfo.key
-      if (typeof key !== 'string' || key.length < 1) throw new Error('Argument key is empty')
-      else if (this.args.unnamed.some(a => a.key === key)) throw new Error('Argument name already exists')
-
-      // type
-      if (!this._validateType(argInfo.type)) argInfo.type = null
-
-      // NOTE: description
-      // if empty, return null to use locale parsing at execution
-      const description = typeof argInfo.description === 'string' && argInfo.description.length > 0
-        ? argInfo.description
-        : null
-
-      // optional field
-      const optional = Boolean(argInfo.optional)
-
-      // infinity field
-      const infinity = Boolean(argInfo.infinity)
-
-      this.args.unnamed.push({
-        key,
-        type: argInfo.type,
-        description,
-        optional,
-        default: argInfo.default,
-        infinity
-      })
+      this.registerArgument(argInfo)
     }
   }
 
@@ -133,8 +147,7 @@ class ArgumentCollector {
    * @returns {boolean}
    */
   has (name) {
-    return name in this.args.named ||
-      this.args.unnamed.some(el => el.key === name)
+    return this.args.some(el => el.key === name)
   }
 }
 
