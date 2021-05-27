b1252bdbf6fb5040a5f3982794b567d4936587cb
My Name <comjun04@gmail.com>
2021-05-27 12:54:15 +0900

    version: v1.0-alpha.5

--- src/bot.js
+++ src/bot.js
@@ -4,8 +4,8 @@
  */
 
 /* eslint-disable no-unused-vars */
-const VERSION = 'v1.0-alpha.4'
-const BUILD_DATE = '2021/5/8'
+const VERSION = 'v1.0-alpha.5'
+const BUILD_DATE = '2021/5/27'
 /* eslint-enable no-unused-vars */
 
 // Setup logger first
