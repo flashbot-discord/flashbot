e296f5e939bddb1716804c7cda8250e84f18c779
comjun04 <comjun04@gmail.com>
2021-09-14 09:07:50 +0900

    chore(version): 2.0.0-alpha.1-dev

--- src/bot.js
+++ src/bot.js
@@ -4,8 +4,8 @@
  */
 
 /* eslint-disable no-unused-vars */
-const VERSION = 'v1.0-beta.4'
-const BUILD_DATE = '2021/8/14'
+const VERSION = 'v2.0-alpha.1-dev'
+const BUILD_DATE = '2021/9/14'
 /* eslint-enable no-unused-vars */
 
 // Setup logger first
